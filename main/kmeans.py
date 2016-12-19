d94945ec7910df9eb6064a0061f3d746b74a4fca
tomekgancarczyk <tomek.gancarczyk@tylko.com>
2016-12-19 20:01:16 +0100

    centroid func get vectors as base for calculations

--- main/kmeans.py
+++ main/kmeans.py
@@ -9,7 +9,7 @@ for c in range(c_num):
 
 # print centroid_x
 # print centroid_y
-dist_x_1 = []
+dist_x = []
 dist_y = []
 for c in centroid_x:
     pass
@@ -29,24 +29,25 @@ def euclidean_distance(v1, v2):
     :return: euclidean distance
     """
     sub = []
-    for i, f in enumerate(v1):
+    for i, feat in enumerate(v1):
         sub.append(math.pow((v1[i] - v2[i]), 2))
     dist = math.sqrt(sum(sub))
     return dist
 
 
-def generate_centroids(c_shape):
+def generate_centroids(c_number, vectors=None):
     """
     generate number od centroid vectors in c_dim-dimensional space of features
     :param c_shape: (number of centroids, dimensions number)
     :return: centroid vectors
     """
+    c_shape = (c_number, len(vectors[0]))
     centroids = []
     for n in range(c_shape[0]):
         _c = []
         for r in range(c_shape[1]):
             _c.append(random.random())
-        _cc = {'c': _c, 'label': n }
+        _cc = {'centroid': _c, 'name': n}
         centroids.append(_cc)
     return centroids
 
@@ -57,17 +58,25 @@ def cluster_points(points, centroids):
         _d = {'v': None, 'd': []}
         for c in centroids:
             _d['d'].append(euclidean_distance(p, c))
-        _c.append(min(pp['d'] for pp in _d))
+        _c.append(min(x['d'] for x in _d))
     return _c
-print generate_centroids([2, 2]), 'centroids'
-centroids = generate_centroids([2, len(points[0])])
-print cluster_points(points, centroids)
 
-for p in points:
-    _dist = []
-    for c in generate_centroids([2, len(p)]):
-        _d = euclidean_distance(p, c['c'])
-        _dist.append(_d)
+# dla dazdego pkt sprawdz dystans od centroidu
+# przypisz pkt do grupy o nazwie centroidu
+# dla grupy wygeneruj na nowo centroid
+# sprawdz dystans pkt od nowych centroidow
+# przypisz na nowo
+
+
+print generate_centroids(2, points), 'centroids'
+centroids = generate_centroids(2, points)
+# print cluster_points(points, centroids)
+
+# for p in points:
+#     _dist = []
+#     for c in generate_centroids([2, len(p)]):
+#         _d = euclidean_distance(p, c['centroid'])
+#         _dist.append(_d)
 
 
 
