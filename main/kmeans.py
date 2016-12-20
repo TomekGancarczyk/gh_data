93a204804d8cefc4622c7fe74e9096c4787b962d
tomekgancarczyk <tomek.gancarczyk@tylko.com>
2016-12-20 21:55:36 +0100

    class vector added to repo

--- main/kmeans.py
+++ main/kmeans.py
@@ -1,25 +1,6 @@
 import math
 import random
 
-centroid_x = []
-centroid_y = []
-for c in range(c_num):
-    centroid_x.append(random.random())
-    centroid_y.append(random.random())
-
-# print centroid_x
-# print centroid_y
-dist_x = []
-dist_y = []
-for c in centroid_x:
-    pass
-for f in feat_1:
-    dist_x.append(abs(f - centroid_x[0]))
-    dist_y.append(abs(f - centroid_x[1]))
-# print dist_x
-points = zip(feat_1, feat_2)
-# print points
-
 
 def euclidean_distance(v1, v2):
     """
@@ -35,7 +16,7 @@ def euclidean_distance(v1, v2):
     return dist
 
 
-def generate_centroids(c_number, vectors=None):
+def initialize_centroids(c_number, vectors=None):
     """
     generate number od centroid vectors in c_dim-dimensional space of features
     :param c_shape: (number of centroids, dimensions number)
@@ -47,10 +28,25 @@ def generate_centroids(c_number, vectors=None):
         _c = []
         for r in range(c_shape[1]):
             _c.append(random.random())
-        _cc = {'centroid': _c, 'name': n}
-        centroids.append(_cc)
+        _c_vec = Vector(_c)
+        _c_vec.set_label(n)
+        centroids.append(_c_vec)
     return centroids
-#
+
+
+def calculate_centroid(vectors=None):
+    """
+    generate number od centroid vectors in c_dim-dimensional space of features
+    :param c_shape: (number of centroids, dimensions number)
+    :return: centroid vectors
+    """
+    features = [x.features for x in vectors]
+
+    v_f = map(sum, zip(*features))
+    centroid = Vector([y / len(vectors) for y in v_f])
+    return centroid
+
+
 def cluster_points(points, centroids):
     clusters = []
     for p in points:
@@ -62,9 +58,32 @@ def cluster_points(points, centroids):
         clusters.append(c)
     return clusters
 
-# def cluster_points(points, centroids):
-#     for centr in centroids:
 
+class Vector(object):
+
+    def __init__(self, features):
+        self.features = features
+        self.label = None
+        self.dimension = len(features)
+
+    def set_label(self, label):
+        self.label = label
+
+    @classmethod
+    def euclidean_distance(cls, v1, v2):
+        """
+        calculates distance between two vectors in n-dimensional space of features (vector parameters)
+        :param v1: vector object
+        :param v2: vector object
+        :return: euclidean distance
+        """
+        sub = []
+        feat1 = v1.features
+        feat2 = v2.features
+        for i, feat in enumerate(feat1):
+            sub.append(math.pow((feat1[i] - feat2[i]), 2))
+        dist = math.sqrt(sum(sub))
+        return dist
 
 
 # dla dazdego pkt sprawdz dystans od centroidu
@@ -72,32 +91,45 @@ def cluster_points(points, centroids):
 # dla grupy wygeneruj na nowo centroid
 # sprawdz dystans pkt od nowych centroidow
 # przypisz na nowo
+centroid_x = []
+centroid_y = []
+for c in range(c_num):
+    centroid_x.append(random.random())
+    centroid_y.append(random.random())
 
+# print centroid_x
+# print centroid_y
+dist_x = []
+dist_y = []
+# print dist_x
+points = zip(feat_1, feat_2)
+vectors = []
+for i in points:
+    _vec = Vector(i)
+    vectors.append(_vec)
+# print points
 cluster_num = 2
 # print generate_centroids(cluster_num, points), 'centroids'
-centroids = generate_centroids(2, points)
-
-# print cluster_points(points, centroids)
-
-
-centr = generate_centroids(2, points)
-clusters = cluster_points(points, centr)
+centroids = initialize_centroids(c_num, points)
+new_centr = calculate_centroid(vectors)
+print new_centr.features, 'new centr'
+centr = new_centr.features
+# clusters = cluster_points(points, centr)
 # print clusters, 'clusters'
-c_points = [x['points'] for x in clusters]
-labels = [y['label'] for y in clusters]
-for j in range(cluster_num):
-    for c in clusters:
-        if c['label'] == j:
-            pass
-centr_labels = []
-centroids = []
-for c in centr:
-    print c['centroid'][0]
-    print c['centroid'][1]
-    print c['name']
+# c_points = [x['points'] for x in clusters]
+# labels = [y['label'] for y in clusters]
+# for j in range(cluster_num):
+#     for c in clusters:
+#         if c['label'] == j:
+#             pass
+# centr_labels = []
+# centroids = []
+# for c in centr:
+#     print c['centroid'][0]
+#     print c['centroid'][1]
+#     print c['name']
 
 
-centroids = [x['centroid'] for x in centr]
 
 
 # for p in points:
